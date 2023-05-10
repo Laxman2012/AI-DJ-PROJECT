6d85fb106591ac6ecb36a66c933063869cd657ee
Laxman2012 <110541419+Laxman2012@users.noreply.github.com>
2023-05-10 16:18:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 hope = "";
 bones = "";
+leftWristx = "";
+leftWristy = "";
+rightWristx = "";
+rightWristy = "";
+hope_status = "";
+bones_status = "";
 
 function preload()
 {
@@ -18,6 +24,9 @@ function setup()
    video = createCapture(VIDEO);
    video.hide();
 
+   poseNet = ml5.poseNet(video , modelLoaded);
+   poseNet.on('pose' , gotPoses);
+
 
 }
 
@@ -25,6 +34,35 @@ function draw()
 {
 
    image(video , 0 , 0 , 600 , 500);
+
+}
+
+function modelLoaded()
+{
+
+  console.log('PoseNet is initialized');
+
+}
+
+function gotPoses(results)  
+{
+
+  if(results.length > 0)
+  {
+
+     console.log(results);
+     scoreRightWrist = results[0].pose.keypoints[10].score;
+     scoreLeftWrist = results[0].pose.keypoints[9].score;
+     console.log("scoreRightWrist =" + scoreRightWrist +"scoreLeftWrist = " + scoreLeftWrist)
+
+     leftWristx = results[0].pose.leftWrist.x;
+     leftWristy = results[0].pose.leftWrist.y;
+     console.log("leftWristX ="+leftWristx+"leftWristy ="+leftWristy);
+     rightWristx = results[0].pose.rightWrist.x;
+     rightWristy = results[0].pose.rightWrist.y;
+     console.log("rightWristX ="+rightWristx+"rightWristy ="+rightWristy);
+
+  }
 
 }
 
